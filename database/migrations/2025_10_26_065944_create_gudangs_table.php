e19411d5d2f808c8075cac9f5e9a49d7ac7a6b37
syakadev <syakadev@gmail.com>
2025-10-26 14:25:27 +0700

    membuat migration gudang

--- database/migrations/2025_10_26_065944_create_gudangs_table.php
+++ database/migrations/2025_10_26_065944_create_gudangs_table.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('gudangs', function (Blueprint $table) {
             $table->id();
+            $table->string('nama');
+            $table->text('alamat');
             $table->timestamps();
         });
     }
