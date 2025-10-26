21a5da09c269346eb97168684a75c4a256f84f37
syakadev <syakadev@gmail.com>
2025-10-26 14:25:46 +0700

    membuat migration  transaksi

--- database/migrations/2025_10_26_070033_create_transaksis_table.php
+++ database/migrations/2025_10_26_070033_create_transaksis_table.php
@@ -13,6 +13,24 @@ return new class extends Migration
     {
         Schema::create('transaksis', function (Blueprint $table) {
             $table->id();
+            $table->text('catatan')->nullable();
+            $table->dateTime('tanggal');
+
+            // 'jumlah' di ERD saya asumsikan sebagai total harga transaksi
+            $table->decimal('total_harga', 15, 2);
+
+            $table->string('alamat_pengiriman')->nullable();
+            $table->enum('jenis_transaksi', ['masuk', 'keluar']); //  'masuk', 'keluar'
+
+            // Relasi 'mencatat' (N-to-1 dengan User)
+            $table->foreignId('user_id')
+                ->constrained('users')
+                  ->onDelete('restrict'); // Jangan hapus user jika pernah mencatat transaksi
+
+            // Relasi 'melakukan' (N-to-1 dengan Pelanggan)
+            $table->foreignId('pelanggan_id')
+                ->constrained('pelanggan')
+                  ->onDelete('restrict'); // Jangan hapus pelanggan jika punya riwayat transaksi
             $table->timestamps();
         });
     }
