0ee9deafe8a3cfd4c84410457cd72b6ea22c14b6
syakadev <syakadev@gmail.com>
2025-10-26 18:13:38 +0700

    membuat user factory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -25,10 +25,10 @@ class UserFactory extends Factory
     {
         return [
             'name' => fake()->name(),
-            'email' => fake()->unique()->safeEmail(),
-            'email_verified_at' => now(),
+            'username' => fake()->unique()->userName(),
             'password' => static::$password ??= Hash::make('password'),
-            'remember_token' => Str::random(10),
+            'phone_number' => fake()->phoneNumber(),
+            'role' => fake()->randomElement(['Admin', 'Warehouse Manager', 'Warehouse Staff', 'Cashier']),
         ];
     }
 
