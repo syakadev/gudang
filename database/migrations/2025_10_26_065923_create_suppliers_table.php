23536ed7329ab740c7c385f1d186d0442a139f72
syakadev <syakadev@gmail.com>
2025-10-26 14:06:18 +0700

    membuat migration suplier

--- database/migrations/2025_10_26_065923_create_suppliers_table.php
+++ database/migrations/2025_10_26_065923_create_suppliers_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('suppliers', function (Blueprint $table) {
             $table->id();
+            $table->string('nama');
+            $table->text('alamat');
+            $table->string('no_telp')->nullable();
             $table->timestamps();
         });
     }
