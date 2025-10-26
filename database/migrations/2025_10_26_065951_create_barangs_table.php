e28f4739f3a0ead23ff22f7e4d4d0cc13b92fcb2
syakadev <syakadev@gmail.com>
2025-10-26 14:25:35 +0700

    membuat migration barang

--- database/migrations/2025_10_26_065951_create_barangs_table.php
+++ database/migrations/2025_10_26_065951_create_barangs_table.php
@@ -13,6 +13,28 @@ return new class extends Migration
     {
         Schema::create('barangs', function (Blueprint $table) {
             $table->id();
+            $table->string('nama_barang');
+            $table->decimal('harga', 15, 2); // 15 digit total, 2 di belakang koma
+            $table->string('kategori');
+            $table->string('satuan'); // misal: 'pcs', 'kg', 'liter'
+            $table->unsignedInteger('stok')->default(0);
+
+            // Relasi 'menyimpan' (N-to-1 dengan Gudang)
+            $table->foreignId('gudang_id')
+                ->constrained('gudang')
+                  ->onDelete('cascade'); // Jika gudang dihapus, barang di dalamnya ikut terhapus
+
+            // Relasi 'menyediakan' (N-to-1 dengan Supplier)
+            $table->foreignId('supplier_id')
+                ->constrained('supplier')
+                  ->onDelete('restrict'); // Jangan hapus supplier jika masih punya barang
+
+            // Siapa user manager gudang yang mengatur barang
+            $table->foreignId('user_id')
+                ->nullable()
+                ->unique()
+                ->constrained('users')
+                  ->onDelete('set null'); // Jika user dihapus, gudang tidak terhapus
             $table->timestamps();
         });
     }
