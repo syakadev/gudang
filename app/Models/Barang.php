93ba30c7c23c2844daf16c1a1346096f8662b031
syakadev <syakadev@gmail.com>
2025-10-26 15:16:31 +0700

    membuat model barang

--- app/Models/Barang.php
+++ app/Models/Barang.php
@@ -2,9 +2,45 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Barang extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'nama_barang',
+        'harga',
+        'kategori',
+        'satuan',
+        'stok',
+        'gudang_id',
+        'supplier_id',
+        'user_id',
+    ];
+
+    protected $casts = [
+        'harga' => 'decimal:2',
+    ];
+
+    public function gudang()
+    {
+        return $this->belongsTo(Gudang::class);
+    }
+
+    public function supplier()
+    {
+        return $this->belongsTo(Supplier::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function detailTransaksis()
+    {
+        return $this->hasMany(DetailTransaksi::class);
+    }
 }
