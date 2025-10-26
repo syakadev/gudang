f2816c3a2591020df32a094d70775b2b651829ec
syakadev <syakadev@gmail.com>
2025-10-26 14:25:18 +0700

    membuat migration pelanggan

--- database/migrations/2025_10_26_065942_create_pelanggans_table.php
+++ database/migrations/2025_10_26_065942_create_pelanggans_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('pelanggans', function (Blueprint $table) {
             $table->id();
+            $table->string('nama');
+            $table->string('no_telp')->nullable();
+            $table->text('alamat');
             $table->timestamps();
         });
     }
